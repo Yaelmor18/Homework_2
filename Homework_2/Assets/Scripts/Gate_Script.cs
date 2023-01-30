a9e6f2047dc5d94e2f2e84d0b3e0a9df8c376f70
Yaelmor18 <yaelmor18@gmail.com>
2023-01-30 15:04:42 +0200

    Open Gate Event
    YASSS

--- Homework_2/Assets/Scripts/Gate_Script.cs
+++ Homework_2/Assets/Scripts/Gate_Script.cs
@@ -6,8 +6,16 @@ public class Gate_Script : MonoBehaviour
 {
     [SerializeField] Animator animator;
 
+    private void Start()
+    {
+    }
     private void OpenGate()
     {
         animator.enabled = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.openGateEvent += OpenGate;
     }
 }
